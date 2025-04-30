73ba1ac23f4f65be5acec9d04bd14082a9b57c6a
David Saavedra Varela <okcee@hotmail.com>
2025-04-30 23:18:33 +0200

    d300425 .4

---
+++ Curso_Testing/Codigo Curso Python/3 - Primeros Pasos/7 Debanado de cadenas.py
@@ -0,0 +1,6 @@
+var = "Monty Python"
+
+print("Conjunto [0:5]: ", var[0:5]) # Salida: Monty
+print("Conjunto [:3]: ", var[:3]) # Salida: Mon
+print("Conjunto: [3:] o [3]", var[3:]) # Salida: ty Python
+print("Conjunto: [-3]", var[-3:]) # Salida: hon
