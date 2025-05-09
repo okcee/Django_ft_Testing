7eaeeedfcedb3a19d628d049bc1d3c8cbb03992c
David Saavedra Varela <okcee@hotmail.com>
2025-05-09 18:30:40 +0200

    d090525 .3

--- Curso_Testing/Testing_Udemy_XPATH/Elements.py
+++ Curso_Testing/Testing_Udemy_XPATH/Elements.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time
 
 driver = webdriver.Chrome("S:\\_Django_ft_Testing\\Curso_Testing\\Drivers\\chromedriver.exe")
 driver.get("https://www.udemy.com/join/login-popup/?locale=es_ES&response_type=html&next=https%3A%2F%2Fwww.udemy.com%2F")
 driver.maximize_window()
 
-varios = driver.find_elements_by_class_name("form-control")
+varios = driver.find_element(By.CLASS_NAME, "form-control")
 for i in varios:
     time.sleep(1)
     i.send_keys("123456789")
