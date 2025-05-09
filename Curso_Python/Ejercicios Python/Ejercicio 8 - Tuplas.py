18e60f683da9c4b950d3d7f1d4adf8c439779073
David Saavedra Varela <okcee@hotmail.com>
2025-05-09 19:28:44 +0200

    d090525 .5

--- Curso_Python/Ejercicios Python/Ejercicio 8 - Tuplas.py
+++ Curso_Python/Ejercicios Python/Ejercicio 8 - Tuplas.py
@@ -1,3 +1,7 @@
 ''' Ejercicio 8: Conjunto de datos - Tuplas
-
+Escribir una tupla con los meses del año, luego, pide al usuario un numero, el que haya ingresado, es el mes que debe mostrar en la tupla
 '''
+meses = ('Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre')
+num = int(input("Introduce el número del mes para saber a cuál corresponde (Entre 1 - 12)"))
+
+print(f'El mes {num} es {meses[num-1]}')
--- Curso_Python/Ejercicios Python/Ejercicio 9 - Diccionarios.py
+++ Curso_Python/Ejercicios Python/Ejercicio 9 - Diccionarios.py
@@ -1,3 +1,23 @@
 ''' Ejercicio 9: Conjunto de datos - Diccionario
+En el siguiente diccionario se encuentran capitales de los paises en el mundo, debes realizar un programa que pida un pais al usuario, y muestre la capital de ese pais, en dado caso el pais no este en el diccionario, se debe mostrar un mensaje diciendo que ese pais no se encuentra.
 
+{"Guatemala": "Ciudad de Guatemala", "El Salvador": "San Salvador", "Honduras": "Honduras","Nicaragua": "Managua", "Costa Rica": "San Jose", "Panama": "Panama", "Argentina": "Buenos Aires", "Colombia": "Bogota", "Venezuela": "Caracas", "España": "Madrid"}
 '''
+
+diccionario = {"Guatemala": "Ciudad de Guatemala", "El Salvador": "San Salvador", "Honduras": "Honduras","Nicaragua": "Managua", "Costa Rica": "San Jose", "Panama": "Panama", "Argentina": "Buenos Aires", "Colombia": "Bogota", "Venezuela": "Caracas", "España": "Madrid"}
+
+print('''
+    Escoge y ecribe uno de los siguientes países para saber su capital:
+        - Guatemala
+        - El Salvador
+        - Honduras
+        - Nicaragua
+        - Costa Rica
+        - Panama
+        - Argentina
+        - Colombia
+        - Venezuela
+        - España
+    ''')
+pais = input()
+print(f'La capital de {pais} es: {diccionario[pais]}')
