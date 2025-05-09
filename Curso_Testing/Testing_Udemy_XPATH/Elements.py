5a82784b92f4fd08dced820cb1b00042b05237e3
David Saavedra Varela <okcee@hotmail.com>
2025-05-09 18:24:34 +0200

    d090525 .2

--- Curso_Testing/Testing_Udemy_XPATH/Elements.py
+++ Curso_Testing/Testing_Udemy_XPATH/Elements.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 import time
 
-driver = webdriver.Chrome("Drivers/chromedriver.exe")
+driver = webdriver.Chrome("S:\\_Django_ft_Testing\\Curso_Testing\\Drivers\\chromedriver.exe")
 driver.get("https://www.udemy.com/join/login-popup/?locale=es_ES&response_type=html&next=https%3A%2F%2Fwww.udemy.com%2F")
 driver.maximize_window()
 
