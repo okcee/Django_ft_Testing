993a5e52163d4b81a9c610c7c99882439ef2d313
David Saavedra Varela <okcee@hotmail.com>
2025-05-10 23:07:23 +0200

    d100525 .7

--- Curso_Python/Ejercicios Python/Ejercicio 17 - POO.py
+++ Curso_Python/Ejercicios Python/Ejercicio 17 - POO.py
@@ -1,3 +1,4 @@
 ''' Ejercicio 17: Programación Orientada a Objetos en Python
-
+Realizar un programa en el cual se declaren dos valores enteros por teclado utilizando el método __init__. Calcular después la suma, resta, multiplicación y división. Utilizar un método para cada una e imprimir los resultados obtenidos. Llamar a la clase Calculadora.
 '''
+
--- Curso_Python/Ejercicios Python/Ejercicio 18 - POO.py
+++ Curso_Python/Ejercicios Python/Ejercicio 18 - POO.py
@@ -1,3 +1,4 @@
 ''' Ejercicio 18: Programación Orientada a Objetos en Python
-
+Escribir una clase que se llame Areas(). A partir de un constructor se deben pasar los valores de Base y Altura. Luego, se debe calcular area de un cuadrado, triangulo y pentagono
 '''
+
