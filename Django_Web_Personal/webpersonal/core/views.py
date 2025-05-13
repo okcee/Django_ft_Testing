c42c75f62e2bdb2e7c2e8d627ebb0a12620d36de
David Saavedra Varela <okcee@hotmail.com>
2025-05-13 11:45:35 +0200

    d130425 .1

--- Django_Web_Personal/webpersonal/core/views.py
+++ Django_Web_Personal/webpersonal/core/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+html_base = """
+    <h1>Mi Web Personal</h1>
+    <ul>
+        <li><a href="/">Portada</a></li>
+        <li><a href="/about/">Acerca de</a></li>
+    </ul>
+"""
+
 def home(request):
-    return HttpResponse("<h1>Mi web personal</h1><h2>Portada</h2>")
+    return HttpResponse(html_base + """
+        <h2>Bienvenidos</h2>
+        <p>Esto es la portada.</p>
+    """)
+
+def about(request):
+    return HttpResponse(html_base + """
+        <h2>Acerca de</h2>
+        <p>Me llamo Héctor y me encanta Django!</p>
+    """)
